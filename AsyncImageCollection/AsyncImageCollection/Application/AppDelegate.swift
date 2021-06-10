6eca9aa84a95e4379ace1845ce66ed5a916e918c
Marco <mdelgiudice@deloitte.it>
2021-06-10 20:10:01 +0200

    setup project

---
+++ AsyncImageCollection/AsyncImageCollection/Application/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  AsyncImageCollection
+//
+//  Created by Del Giudice, Marco on 10/06/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+        self.window = window
+        return true
+    }
+    
+}
+
