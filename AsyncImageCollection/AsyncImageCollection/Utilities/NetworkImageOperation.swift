2312bdfd22ac860d3542fba4c3cc11145083be4e
Marco <mdelgiudice@deloitte.it>
2021-06-10 20:37:52 +0200

    NetworkImageOperation

---
+++ AsyncImageCollection/AsyncImageCollection/Utilities/NetworkImageOperation.swift
@@ -0,0 +1,60 @@
+//
+//  NetworkImageOperation.swift
+//  AsyncImageCollection
+//
+//  Created by Del Giudice, Marco on 10/06/21.
+//
+
+import Foundation
+import UIKit
+import Alamofire
+
+final class NetworkImageOperation: AsyncOperation {
+    var image: UIImage?
+    
+    private let url: URL
+    private let completionHandler: ((UIImage?) -> Void)?
+    private var task: DataRequest?
+    
+    init(url: URL, completionHandler: ((UIImage?) -> Void)? = nil) {
+        self.url = url
+        self.completionHandler = completionHandler
+        
+        super.init()
+    }
+    
+    convenience init?(string: String, completionHandler: ((UIImage?) -> Void)? = nil) {
+        guard let url = URL(string: string) else { return nil }
+        self.init(url: url, completionHandler: completionHandler)
+    }
+    
+    override func main() {
+        let task = NetworkManager.shared.session.request(url)
+        task.response { [weak self] (response) in
+            guard let self = self else { return }
+            defer { self.state = .finished }
+            guard !self.isCancelled else { return }
+            switch response.result {
+            case .success(let data):
+                guard let data = data else { return }
+                self.image = UIImage(data: data)
+                self.callCompletionHandler(self.image)
+            case .failure(_):
+                self.callCompletionHandler(nil)
+            }
+        }
+    }
+    
+    private func callCompletionHandler(_ image: UIImage?) {
+        if let completionHandler = self.completionHandler {
+            completionHandler(self.image)
+            return
+        }
+    }
+    
+    override func cancel() {
+        super.cancel()
+        task?.cancel()
+    }
+}
+
